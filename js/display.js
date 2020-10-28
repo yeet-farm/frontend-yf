c773ede1be1522c4ca8ef165eb0f6e26ac418adf
ChillerDragon <chillerdragon@gmail.com>
2020-10-28 16:35:02 +0100

    Some styling seems like we go with standard

--- js/display.js
+++ js/display.js
@@ -3,45 +3,42 @@
 /* This class hasn't changed much since part 1. All it does now is resize the canvas
 a bit differently and draw rectangles to the buffer. */
 
-
-
 const Display = function(canvas) {
-  this.buffer  = document.createElement("canvas").getContext("2d");
-  this.context = canvas.getContext("2d");
-  this.cropTexture = new Image();
-  this.cropTexture.src = "img/dirt.svg";
-  
-  this.drawRectangle = function(x, y, width, height, color) {
-    this.buffer.fillStyle = color;
-    this.buffer.fillRect(Math.floor(x), Math.floor(y), width, height);
-  };
+  this.buffer = document.createElement('canvas').getContext('2d')
+  this.context = canvas.getContext('2d')
+  this.cropTexture = new Image()
+  this.cropTexture.src = 'img/dirt.svg'
 
-  this.drawCrop = function(x, y) {
+  this.drawRectangle = function (x, y, width, height, color) {
+    this.buffer.fillStyle = color
+    this.buffer.fillRect(Math.floor(x), Math.floor(y), width, height)
+  }
+
+  this.drawCrop = function (x, y) {
     this.buffer.drawImage(this.cropTexture, x, y, 16, 16);
   }
-  
-  this.fill = function(color) {
-    this.buffer.fillStyle = color;
-    this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
-  };
-  
-  this.render = function() {
-    this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height);
-  };
-  
-  this.resize = function(width, height, height_width_ratio) {
-    if (height / width > height_width_ratio) {
-      this.context.canvas.height = width * height_width_ratio;
-      this.context.canvas.width = width;
-    } else {
-      this.context.canvas.height = height;
-      this.context.canvas.width = height / height_width_ratio;
-    }
-    this.context.imageSmoothingEnabled = false;
-  };
 
-};
+  this.fill = function (color) {
+    this.buffer.fillStyle = color
+    this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
+  }
+
+  this.render = function () {
+    this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height);
+  }
+
+  this.resize = function(width, height, heightWidthRatio) {
+    if (height / width > heightWidthRatio) {
+      this.context.canvas.height = width * heightWidthRatio
+      this.context.canvas.width = width
+    } else {
+      this.context.canvas.height = height
+      this.context.canvas.width = height / heightWidthRatio
+    }
+    this.context.imageSmoothingEnabled = false
+  }
+}
 
 Display.prototype = {
-  constructor : Display
-};
+  constructor: Display
+}
--- js/index.js
+++ js/index.js
@@ -8,38 +8,37 @@
 //     .then(data => console.log(data))
 //     .catch(error => console.log(`ERROR: ${error}`));
 
-var TILE_SIZE = 32;
+var TILE_SIZE = 32
 
-
-function render() {
-    display.fill("#000000ff");
-    // display.drawRectangle(2, 2, 10, 10, "#00ff00ff");
-    for (let i = 0; i < TILE_SIZE/2; i++)
-    {
-        let x = i*TILE_SIZE;
-        // console.log(x)
-        display.drawCrop(x,0);
-    }
-    display.render();
+function render () {
+  display.fill('#000000ff')
+  // display.drawRectangle(2, 2, 10, 10, '#00ff00ff')
+  for (let i = 0; i < TILE_SIZE / 2; i++)
+  {
+    let x = i * TILE_SIZE
+    console.log(x)
+    display.drawCrop(x,0)
+  }
+  display.render()
 }
 
-function update() {
+function update () {
 }
 
 
 var height = TILE_SIZE * 9;
 var width = TILE_SIZE * 16;
 
-function resize() {
-    display.resize(document.documentElement.clientWidth - 32, document.documentElement.clientHeight - 32, height / width);
-    display.render();
+function resize () {
+  display.resize(document.documentElement.clientWidth - 32, document.documentElement.clientHeight - 32, height / width);
+  display.render()
 }
 
 
-var display    = new Display(document.querySelector("canvas"));
-var engine     = new Engine(1000/30, render, update);
+var display = new Display(document.querySelector('canvas'))
+var engine = new Engine(1000 / 30, render, update)
 
-window.addEventListener("resize",  resize);
+window.addEventListener('resize', resize)
 
-resize();
-engine.start();
+resize()
+engine.start()
