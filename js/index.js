265deee234bc0d33a3b977ab95cf075565b827f1
ChillerDragon <chillerdragon@gmail.com>
2020-10-30 12:51:58 +0100

    Made farm fields clickable

--- js/index.js
+++ js/index.js
@@ -12,6 +12,26 @@ const NUM_COLS = 16
 const HEIGHT = TILE_SIZE * NUM_ROWS
 const WIDTH = TILE_SIZE * NUM_COLS
 
+const FARM = new Array(NUM_COLS).fill(0).map(() => new Array(NUM_ROWS).fill(0))
+
+function getCursorPosition (canvas, event) {
+  const rect = canvas.getBoundingClientRect()
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+  // console.log('x: ' + x + ' y: ' + y + ' canvas.w: ' + canvas.width + ' canvas.h: ' + canvas.height)
+  const tileWidth = canvas.width / NUM_COLS
+  const tileX = Math.floor(x / tileWidth)
+  const tileY = Math.floor(y / tileWidth)
+  // console.log('tileW: ' + tileWidth + ' tileX: ' + tileX + ' tileY: ' + tileY)
+  return [tileX, tileY]
+}
+
+const canvas = document.querySelector('canvas')
+canvas.addEventListener('mousedown', function(e) {
+  const tile = getCursorPosition(canvas, e)
+  FARM[tile[0]][tile[1]] = 1
+})
+
 function render () {
   display.fill('#8a563cff')
   // display.drawRectangle(2, 2, 10, 10, '#00ff00ff')
@@ -22,7 +42,9 @@ function render () {
   }
   for (let x = 0; x < NUM_COLS; x++) {
     for (let y = 1; y < NUM_ROWS; y++) {
-      display.drawCrop(x * TILE_SIZE, y * TILE_SIZE - (TILE_SIZE / 2))
+      if (FARM[x][y] === 1) {
+        display.drawCrop(x * TILE_SIZE, y * TILE_SIZE - (TILE_SIZE / 2))
+      }
     }
   }
   display.render()
